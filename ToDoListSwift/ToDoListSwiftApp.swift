b75c7f05ed918753660f712750e0d8d1831d195c
LouGabian <jonathangusse@gmail.com>
2024-10-02 22:43:59 +0200

    Initial commit

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by Jonathan Gusse on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
