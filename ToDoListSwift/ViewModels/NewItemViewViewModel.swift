a2b78e3a478bc4f6966efce25d144bfedac0edc8
LouGabian <jonathangusse@gmail.com>
2024-10-05 23:49:20 +0200

    Saving To Do List Item

--- ToDoListSwift/ViewModels/NewItemViewViewModel.swift
+++ ToDoListSwift/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,11 @@
 //  Created by Jonathan Gusse on 03/10/2024.
 //
 
+
+
+
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -18,9 +23,42 @@ class NewItemViewViewModel: ObservableObject {
         
     }
     
+    
+    //MARK: Save Func
+    
     func save() {
+        guard canSave else {
+            return
+        }
         
-    }
+        
+        //GET current user
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        //CREATE model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        //SAVE model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionnary())
+        
+        
+    }//END fun save
+    
     
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
