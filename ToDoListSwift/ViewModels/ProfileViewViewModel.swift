8647cba7f39501455ff6b4feb926e674a01ff2e2
LouGabian <jonathangusse@gmail.com>
2024-10-06 03:06:07 +0200

    Done Checkmarks

--- ToDoListSwift/ViewModels/ProfileViewViewModel.swift
+++ ToDoListSwift/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,9 @@
 //  Created by Jonathan Gusse on 03/10/2024.
 //
 
+
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -13,11 +16,26 @@ class ProfileViewViewModel: ObservableObject {
     
     init () {
         
-    }
+    }// END init ...
     
     func toggleIsDone(item: ToDoListItem) {
         
-    }
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionnary())
+        
+    }//END func toggle ...
     
     
-}
+}// END class ...
